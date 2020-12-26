762dc8a7cdf4fd7462b0ceca5a1a30eea55cd52b
lkukuruku <l.kukuruku@gmail.com>
2020-12-26 16:21:38 +0300

    feature-12 - merge from develop

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,11 +5,13 @@ import {
     Route,
 } from 'react-router-dom';
 import Home from 'Pages/Home';
+import Account from 'Pages/Account';
 
 const App = () => (
     <Router>
         <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/account" component={Account} />
             <Route component={() => <div>404 placeholder</div>} />
         </Switch>
     </Router>
