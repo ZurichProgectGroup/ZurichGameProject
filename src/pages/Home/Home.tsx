12208cd9218a2bb4439073b6667138a51aa36367
Viktor Dorofeev <dorofeevvictorv@gmail.com>
2021-01-12 20:41:54 +0300

    Изменил навигацию на главном экране

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import './Home.css';
 import logo from 'Images/logo.png';
-import cup from 'Images/cup.png';
-import { Avatar, Button, LinkButton } from 'Components';
+import { Avatar, Button } from 'Components';
 import { ButtonSize, ButtonVariant } from 'Components/Button/types';
+import NavigationList from 'Components/NavigationList';
+import RouteMap from './const';
 
 const Home = () => (
     <div className="home">
         <div className="home__header">
             <Avatar size="medium" name="igor" url="https://picsum.photos/200" />
-            <LinkButton to="/leaderboard">
-                <img src={cup} alt="cup" />
-            </LinkButton>
+            <NavigationList routes={RouteMap} />
         </div>
         <img className="home__logo" src={logo} width={685} height={320} alt="logo" />
         <Button
