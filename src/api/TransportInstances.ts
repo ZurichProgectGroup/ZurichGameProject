01622aafb0a33e4713ddeecc0621a32e8630decc
max <animan98@gmail.com>
2021-04-29 01:52:40 +0500

    update api routes

--- src/api/TransportInstances.ts
+++ src/api/TransportInstances.ts
@@ -1,9 +1,11 @@
 import HTTPTransport from 'utils/http_transport/http_transport';
 
+const BASE_PATH = `${window.location.origin}/api`;
+
 export const authTransportInstance = new HTTPTransport('https://ya-praktikum.tech/api/v2/auth');
 export const leaderboardTransportInstance = new HTTPTransport('https://ya-praktikum.tech/api/v2/leaderboard');
 export const oauthTransportInstance = new HTTPTransport('https://ya-praktikum.tech/api/v2/oauth');
 export const userTransportInstance = new HTTPTransport('https://ya-praktikum.tech/api/v2/user');
-export const topicsTransportInstance = new HTTPTransport('https://local.ya-praktikum.tech/topics');
-export const commentsTransportInstance = new HTTPTransport('https://local.ya-praktikum.tech/comments');
-export const userThemesInstance = new HTTPTransport('https://local.ya-praktikum.tech/user-themes');
+export const topicsTransportInstance = new HTTPTransport(`${BASE_PATH}/topics`);
+export const commentsTransportInstance = new HTTPTransport(`${BASE_PATH}/comments`);
+export const userThemesInstance = new HTTPTransport(`${BASE_PATH}/user-themes`);
